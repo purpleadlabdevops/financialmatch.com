db3ea75de659994902e2bd5f7472ed828e672f6b
Микола Рижков <nickr@MacBook-Pro-Mikola.local>
2022-08-08 14:59:30 +0300

    some fix

--- static/js/everflow.js
+++ static/js/everflow.js
@@ -1,16 +1,10 @@
-setTimeout(() => {
-  EF.click({
-    offer_id: 1,
-    affiliate_id: EF.urlParameter('affid'),
-    uid: EF.urlParameter('uid'),
-    source_id: EF.urlParameter('source_id'),
-  })
-  EF.conversion({
-    offer_id: 1,
-    event_id: 2,
-  })
-}, 2000)
 
+EF.click({
+  offer_id: 1,
+  affiliate_id: EF.urlParameter('affid'),
+  uid: EF.urlParameter('uid'),
+  source_id: EF.urlParameter('source_id'),
+})
 setTimeout(() => {
   EF.conversion({
     offer_id: 1,
